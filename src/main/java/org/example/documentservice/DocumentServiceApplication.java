15ca6b3a2e455a5bee0e7dcafb1f1b7c6ccf4d78
Megamind <soy.tola9999@gmail.com>
2024-10-20 22:09:42 +0700

    change elastic

--- src/main/java/org/example/documentservice/DocumentServiceApplication.java
+++ src/main/java/org/example/documentservice/DocumentServiceApplication.java
@@ -5,12 +5,15 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
 @OpenAPIDefinition(
+        servers = @Server(url = "/"),
         info = @Info(
                 title = "Document-Service",
                 description = "Stack-Notes Api"
@@ -27,6 +30,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
         )
 )
 @EnableDiscoveryClient
+@EnableElasticsearchRepositories(basePackages = "org.example.documentservice.repository")
 public class DocumentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DocumentServiceApplication.class, args);
--- src/main/java/org/example/documentservice/repository/DocumentElasticRepository.java
+++ src/main/java/org/example/documentservice/repository/DocumentElasticRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import java.util.List;
 import java.util.UUID;
 
+
 public interface DocumentElasticRepository extends ElasticsearchRepository<DocumentElasticEntity, UUID> {
     void deleteByWorkspaceId(UUID workspaceId);
     List<DocumentElasticEntity> findAllByWorkspaceId(UUID workspaceId);
