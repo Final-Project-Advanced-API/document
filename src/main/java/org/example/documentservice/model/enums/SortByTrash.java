4ab124e8c945216a6fd3ad5d6d0c769e1e68b874
Megamind <soy.tola9999@gmail.com>
2024-11-05 22:08:23 +0700

    modify feedback

--- src/main/java/org/example/documentservice/model/enums/SortByTrash.java
+++ src/main/java/org/example/documentservice/model/enums/SortByTrash.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SortByTrash {
-	TITLE("title"),
 	CREATED_AT("createdAt"),
 	UPDATED_AT("updatedAt");
 	private final String fieldName;
